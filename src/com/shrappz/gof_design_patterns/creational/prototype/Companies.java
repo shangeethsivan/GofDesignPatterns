c65514dd53aca2cc27acd672fed71f61b50fea80
Shangeeth <shangeeth.sivan@anywhere.co>
2018-07-31 21:06:27 +0530

    Prototype Pattern Added

---
+++ src/com/shrappz/gof_design_patterns/creational/prototype/Companies.java
@@ -0,0 +1,47 @@
+package com.shrappz.gof_design_patterns.creational.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Shangeeth on 31/07/18.
+ */
+public class Companies implements Cloneable{
+
+    private List<String> mCompanieNames;
+
+
+    public Companies(){
+        mCompanieNames = new ArrayList<>();
+    }
+
+
+    private Companies(List<String> pCompanies){
+        mCompanieNames = pCompanies;
+    }
+
+
+    public void loadData(){
+        // Consider that you are reading data from database and adding to the list in this part
+        mCompanieNames.add("Full Creative");
+        mCompanieNames.add("HCL");
+        mCompanieNames.add("TCS");
+        mCompanieNames.add("Google");
+        mCompanieNames.add("Wipro");
+        mCompanieNames.add("Amazon");
+    }
+
+    public List<String> getCompanies(){
+        return mCompanieNames;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        List<String> companienames = new ArrayList<>();
+        for (String lCompanyName :
+                mCompanieNames) {
+            companienames.add(lCompanyName);
+        }
+        return new Companies(companienames);
+    }
+}
---
+++ src/com/shrappz/gof_design_patterns/creational/prototype/PrototypePatternTester.java
@@ -0,0 +1,29 @@
+package com.shrappz.gof_design_patterns.creational.prototype;
+
+/**
+ * Created by Shangeeth on 31/07/18.
+ */
+public class PrototypePatternTester {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        Companies lCompanies = new Companies();
+        lCompanies.loadData();
+        Companies lCompanies1 = (Companies) lCompanies.clone();
+        Companies lCompanies2 = (Companies) lCompanies.clone();
+
+        lCompanies.getCompanies().add(0,"New Company");
+        lCompanies1.getCompanies().remove("Full Creative");
+
+        System.out.println("Data in Original Companies Object: "+lCompanies.getCompanies());
+        System.out.println("Data in Clone 1 Companies Object: "+lCompanies1.getCompanies());
+        System.out.println("Data in Clone 2 Companies Object: "+lCompanies2.getCompanies());
+
+//      If the object cloning was not provided,
+//      we will have to make database call to fetch the employee list every time.
+
+
+
+    }
+
+}
